c75c9f054f3d97c195d22f4f75c7f08e4f9803dc
danierelug <luzdanielareyeslugo@gmail.com>
2024-09-09 11:49:28 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "google" {
   project     = "smart-radio-435114-m1"
   region      = "us-central1"
-  credentials = file("smart-radio-435114-m1-e70024a1f690.json")
 }
 
 resource "google_cloud_run_v2_service" "default" {
