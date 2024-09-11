3151c1946a7a8d5e00f588354130b44d516bba4d
danierelug <luzdanielareyeslugo@gmail.com>
2024-09-11 08:00:29 -0600

    Create file via Google Apps Script

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+    project     = "smart-radio-435114-m1"
+    region      = "us-central1"
+  }
+
+resource "google_cloud_run_v2_service" "default" {
+  name     = "PruebaCR"
+  location = "us-central1"
+  deletion_protection = false
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      resources {
+        limits = {
+          cpu    = "2"
+          memory = "1024Mi"
+        }
+      }
+    }
+  }
+}
